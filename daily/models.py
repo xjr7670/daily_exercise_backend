8c37e16b6108567882cb8a16b1901fb48f8d2ba8
xjr7670 <xjr30226@126.com>
2021-10-25 22:33:46 +0800

    增加2个表

--- daily/models.py
+++ daily/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class iCourse(models.Model):
+    today = models.CharField(max_length=8)
+    watched = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.today + ' - ' + self.watched
+
+
+class Pushup(models.Model):
+    today = models.CharField(max_length=8)
+    finish_num = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.today + ' - ' + self.finish_num
