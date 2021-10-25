7ab9fc00669e2da2c3e934aee68f9710c1899732
xjr7670 <xjr30226@126.com>
2021-10-25 23:36:37 +0800

    修复了更新课程数据的函数

--- daily/views.py
+++ daily/views.py
@@ -1,3 +1,6 @@
+# -*- coding:utf-8 -*-
+
+from django.utils import timezone
 from django.shortcuts import HttpResponse, render
 from .models import iCourse
 
@@ -13,12 +16,25 @@ def receive_data(request):
     post_data = json.loads(post_data)
     print(post_data)
 
+    today = timezone.now().strftime('%Y%m%d')
+    course_data = post_data['result']
+    data_str = ','.join(map(str, course_data))
+
+    filter_result = iCourse.objects.filter(today=today)
+    n = filter_result.count()
+    if n > 0:
+        filter_result.update(**{'today': today, 'watched': data_str})
+    else:
+        i = iCourse(today=today, watched=data_str)
+        i.save()
+    
     return HttpResponse(json.dumps(post_data))
 
 
 def get_data(request):
     """从数据库中取数据返回"""
     watched = iCourse.objects.all()
+    watched = watched.order_by('-today')[0]
     return HttpResponse(watched)
 
 
