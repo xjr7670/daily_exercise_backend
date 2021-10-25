61f45d13890617ff9a585370301d1069ecfd5416
xjr7670 <xjr30226@126.com>
2021-10-25 22:34:40 +0800

    删掉静态文件的设置部分

--- exercise/settings.py
+++ exercise/settings.py
@@ -153,8 +153,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-HERE = os.path.dirname(os.path.abspath(__file__))
-HERE = os.path.join(HERE, '../')
-STATICFILES_DIRS = (
-    os.path.join(HERE, 'static/'),
-)
