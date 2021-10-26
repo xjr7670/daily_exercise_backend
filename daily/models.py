7ee662bee5753b9e1a72e4c18a398a5d57682e3b
root <root@Cavin-Y7000.localdomain>
2021-10-26 22:48:45 +0800

    增加自增主键、课程表增加课程名称

--- daily/models.py
+++ daily/models.py
@@ -2,14 +2,17 @@ from django.db import models
 
 # Create your models here.
 class iCourse(models.Model):
+    id = models.AutoField(primary_key=True, null=False)
     today = models.CharField(max_length=8)
+    name = models.CharField(max_length=100)
     watched = models.CharField(max_length=1000)
 
     def __str__(self):
-        return self.today + ' - ' + self.watched
+        return self.name + ' - ' + self.watched
 
 
 class Pushup(models.Model):
+    id = models.AutoField(primary_key=True, null=False)
     today = models.CharField(max_length=8)
     finish_num = models.IntegerField(default=0)
 
