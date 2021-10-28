658bbf74af9fa6dcf5b4a93b93ed28981d688a7d
cavin <xjr30226@126.com>
2021-10-28 15:45:22 +0800

    把api改为update

--- daily/urls.py
+++ daily/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('api', views.receive_data, name='daily'),
+    path('update', views.receive_data, name='daily'),
     path('post_test', views.post_test, name='test'),
     path('get_data', views.get_data, name='get'),
 ]
