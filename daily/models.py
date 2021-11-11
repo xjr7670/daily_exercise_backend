af7c890d0935e34c0426b8f1b22539f5d50bcad6
cavin <xjr30226@126.com>
2021-11-11 18:17:02 +0800

    add less than compare method and __repr__ to both two model

--- daily/models.py
+++ daily/models.py
@@ -10,6 +10,9 @@ class iCourse(models.Model):
     def __str__(self):
         return self.name + ' - ' + self.watched
 
+    def __repr__(self):
+        return self.name + ' - ' + self.watched
+
 
 class Pushup(models.Model):
     id = models.AutoField(primary_key=True, null=False)
@@ -18,3 +21,12 @@ class Pushup(models.Model):
 
     def __str__(self):
         return self.today + ' - ' + str(self.finish_num)
+
+    def __repr__(self):
+        return self.today + ' - ' + str(self.finish_num)
+
+    def __lt__(self, other):
+        if not isinstance(other, Pushup):
+            raise TypeError('Only compare to Pushup, '
+                            'not other type!')
+        return self.today < other.today
